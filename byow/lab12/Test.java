77ddd51f951ec91f11e428cc981eb57babbc1a3e
Justin Kuo <jkuo74@berkeley.edu>
2019-04-22 11:21:57 -0700

    parameter changes

--- byow/lab12/Test.java
+++ byow/lab12/Test.java
@@ -9,8 +9,8 @@ import java.util.Random;
 
 
 public class Test {
-    private static final int WIDTH = 70;
-    private static final int HEIGHT = 70;
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
 
     private static final long SEED = 2873123;
     private static final Random RANDOM = new Random(SEED);
@@ -20,7 +20,10 @@ public class Test {
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
         TETile[][] world = new TETile[WIDTH][HEIGHT];
-        createWorld(world);
+
+        int numRooms = Math.abs(RANDOM.nextInt()) % (WIDTH + HEIGHT);
+        int numHalls = Math.abs(RANDOM.nextInt()) % (WIDTH + HEIGHT);
+        createWorld(world, numRooms, numHalls);
         ter.renderFrame(world);
     }
 
@@ -28,14 +31,15 @@ public class Test {
      * 1) Initialize all tiles to NOTHING
      * 2) Get random numbers for number of rooms and number of halls to create
      * 3) Add the number of objects to the grid
-     *      a) Room size height and width are limited to at most one tenth of the world's dimension and at least 2x2
-     *         2 <= Room Width <= World Width / 10 //CAN CHANGE IF WE WANT TO
-     *      b) Hallway length is limited to at most a third of the world's width or height depending on it's orientation.
-     *         Orientation is based on returned boolean value of the RNG.
-     *         0 <= Hallway length <= World Height / 3 or 0 <= Hallway length <= World Width
+     * a) Room size height and width are limited to at most one tenth of the world's dimension and at least 2x2
+     * 2 <= Room Width <= World Width / 10 //CAN CHANGE IF WE WANT TO
+     * b) Hallway length is limited to at most a third of the world's width or height depending on it's orientation.
+     * Orientation is based on returned boolean value of the RNG.
+     * 0 <= Hallway length <= World Height / 3 or 0 <= Hallway length <= World Width
+     *
      * @param grid World to create on.
      */
-    public static void createWorld(TETile[][] grid) {
+    public static void createWorld(TETile[][] grid, int numRooms, int numHalls) {
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
                 grid[x][y] = Tileset.NOTHING;
@@ -44,24 +48,23 @@ public class Test {
 
         WeightedQuickUnionUF set = new WeightedQuickUnionUF(WIDTH * HEIGHT + 1);//TODO: make all connected
 
-        int numRooms = Math.abs(RANDOM.nextInt()) % (WIDTH + HEIGHT);
-        int numHalls = Math.abs(RANDOM.nextInt()) % (WIDTH + HEIGHT);
-
         for (int n = 0; n < numRooms; n++) {
-            addArea(grid, set, Math.abs(RANDOM.nextInt()) % ((WIDTH / 10) - 2) + 2,
-                    Math.abs(RANDOM.nextInt()) % ((HEIGHT / 10) - 2) + 2);
+            int width = Math.abs(RANDOM.nextInt()) % ((WIDTH / 10) - 2) + 2;
+            int height = Math.abs(RANDOM.nextInt()) % ((HEIGHT / 10) - 2) + 2;
+            addArea(grid, set, width, height);
         }
+
         for (int n = 0; n < numHalls; n++) {
             boolean orientation = RANDOM.nextBoolean();
-            int x, y;
+            int width, height;
             if (orientation) {
-                x = 1;
-                y = Math.abs(RANDOM.nextInt()) % (HEIGHT / 3);
+                width = 1;
+                height = Math.abs(RANDOM.nextInt()) % (HEIGHT / 3);
             } else {
-                x = Math.abs(RANDOM.nextInt()) % (WIDTH / 3);
-                y = 1;
+                width = Math.abs(RANDOM.nextInt()) % (WIDTH / 3);
+                height = 1;
             }
-            addArea(grid, set, x, y);
+            addArea(grid, set, width, height);
         }
     }
 
@@ -70,9 +73,9 @@ public class Test {
      * 2) Set all points within area to be a FLOOR
      * 3) Bound area with walls if current tile is still NOTHING
      *
-     * @param grid World to create on
-     * @param set Keep track the connectivity of the world
-     * @param width Width of the are to be created
+     * @param grid   World to create on
+     * @param set    Keep track the connectivity of the world
+     * @param width  Width of the are to be created
      * @param height Height of the area to be created
      */
     public static void addArea(TETile[][] grid, WeightedQuickUnionUF set, int width, int height) {
@@ -95,6 +98,7 @@ public class Test {
                 grid[col][LLY + height] = Tileset.WALL;
             }
         }
+
         for (int row = LLY; row < LLY + height && row < HEIGHT; row++) {
             if (LLX - 1 >= 0 && grid[LLX - 1][row] == Tileset.NOTHING) {
                 grid[LLX - 1][row] = Tileset.WALL;
